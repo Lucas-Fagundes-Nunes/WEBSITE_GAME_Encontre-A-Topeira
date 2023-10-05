27209090568acb2669955a07895a9193f6a6c432
LucasNunes <lucasfnunes6@gmail.com>
2023-10-05 09:29:57 -0300

    modificaçoes

--- src/js/engine.js
+++ src/js/engine.js
@@ -2,23 +2,41 @@ let states = {
     view: {
         board: document.getElementById("board"),
         score: document.getElementById("score"),
-    }
+        textScore: document.getElementById("text-score"),
+        divScore: document.getElementById('div-score'),
+    },
+    results: {
+        score: 0,
+        gameOver: false,
+    },
+    currMoleTile: '',
+    currPlantTile: '',
 }
-let currMoleTile;
 
 window.onload = function () {
     setGame();
 }
 
-function setGame() {
-    for (let i = 0; i < 9; i++) {
-        let tile = document.createElement("div");
-        tile.id = i.toString();
+function setPlant() {
+    if (states.results.gameOver) {
+        return;
+    } 
 
-        states.view.board.appendChild(tile)
+    if (states.currPlantTile) {
+        states.currPlantTile.innerHTML = "";
+    }
+    
+    let plant = document.createElement("img");
+    plant.src = "./src/images/piranha-plant.png";
+
+    let num = getRandomTile();
+
+    if (states.currMoleTile && states.currMoleTile.id == num) {
+        return;
     }
 
-    setInterval(setMole, 2000);
+    states.currPlantTile = document.getElementById(num);
+    states.currPlantTile.appendChild(plant)
 }
 
 function getRandomTile () {
@@ -28,13 +46,50 @@ function getRandomTile () {
 
 function setMole() {
 
-    if (currMoleTile) {
-        currMoleTile.innerHTML = "";
+    if (states.results.gameOver) {
+        return;
+    } 
+
+    if (states.currMoleTile) {
+        states.currMoleTile.innerHTML = "";
     }
     let mole = document.createElement("img");
     mole.src = "./src/images/monty-mole.png";
 
     let num = getRandomTile();
-    currMoleTile = document.getElementById(num);
-    currMoleTile.appendChild(mole)
+
+    if (states.currPlantTile && states.currPlantTile.id == num) {
+        return;
+    }
+
+    states.currMoleTile = document.getElementById(num);
+    states.currMoleTile.appendChild(mole)
+}
+
+function selectTile() {
+    if (states.results.gameOver) {
+        return;
+    } 
+
+    if (this == states.currMoleTile) {
+        states.results.score ++
+        states.view.score.innerHTML = states.results.score.toString();
+    } else if (this == states.currPlantTile) {
+        states.view.textScore.innerText = "GAME OVER";
+        states.view.score.innerHTML = states.results.score.toString();
+        states.results.gameOver = true;
+        states.view.divScore.classList.add('div-score');
+    }
+}
+
+function setGame() {
+    for (let i = 0; i < 9; i++) {
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener('click', selectTile);
+        states.view.board.appendChild(tile)
+    }
+
+    setInterval(setMole, 1000);
+    setInterval(setPlant, 2000)
 }
